07adf5b98e02683c205a2796ed95cfd0e6635703
dongyuwei <newdongyuwei@gmail.com>
2012-11-26 19:15:08 +0800

    缓存版本号 ?v=md5-hash

---
+++ index.js
@@ -0,0 +1,69 @@
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var log = require('./lib/tools/log');
+var walk = require('./lib/tools/dirWalker');
+
+var weiboPkgJs = require('./lib/js/PKG/weiboPkgJs');
+var weiboPkgCss = require('./lib/css//PKG/weiboPkgCss');
+
+var cpf = require('./lib/tools/cpFile');
+var writeMappingFile = require('./lib/tools/writeMappingFile');
+
+console.time('Package-Time');
+
+var conf = require("argsparser").parse();
+var from = conf['-from'], to = conf['-to'];
+
+function showUsage(){
+	console.error('Usage: node js/main.js -from fromDir -to toDir [-root srcRoot] [-verbose or -v]');
+	process.exit(1);
+}
+if(!from || !fs.existsSync(from)){
+	console.log('need fromDir');
+	showUsage();
+}
+from = path.join(path.resolve(from),path.sep);
+
+if(!conf['-root']){
+	conf['-root'] = from;
+}
+
+if(!to || !fs.existsSync(to)){
+	console.log('need toDir');
+	showUsage();
+}
+to = path.join(path.resolve(to),path.sep);
+
+//获得打包路径的列表
+console.log('Please wait...\n');
+
+var files = walk(from), jsList = files.js, cssList = files.css, otherFiles = files.other;
+
+//1. 先把目标目录建立好
+var target;
+jsList.concat(cssList).concat(otherFiles).forEach(function(uri){
+	target = uri.replace(from,to);
+	if(!fs.existsSync(path.dirname(target))){
+		mkdirp.sync(path.dirname(target),0777);
+	}
+});
+
+//2. 复制非js,css文件(swf,图片等静态资源,同时计算其md5)
+console.log('Copy files. Please wait...\n');
+otherFiles.forEach(function(source){
+	cpf(source,source.replace(from,to));
+});
+
+//3.压缩,合并css
+weiboPkgCss(from,to,cssList);
+
+//4. 压缩,合并js
+weiboPkgJs(from,to,jsList,conf);
+
+writeMappingFile(from,to);
+
+console.log('######## Package SUCCESS! ###########');
+console.timeEnd('Package-Time');
+
+process.exit(0);
