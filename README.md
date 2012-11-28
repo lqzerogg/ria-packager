df64104906ceb4b6abff024004d3b30a79b386e3
dongyuwei <newdongyuwei@gmail.com>
2012-11-28 19:43:40 +0800

    生成目标模板时自动替换md5版本号

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 #前端打包系统:批量压缩,合并js,css. #
- 1. 合并require('a/b/c.js');
- 2. 合并@import url("a/b/c.css"); 其中会重新计算背景图片相对地址
+ 1. 合并`require('a/b/c.js');`
+ 2. 合并`@import url("a/b/c.css");` 其中会重新计算背景图片相对地址
  3. 计算文件md5值，用于缓存版本号。
-    1. 替换背景图片地址 background-image: url(../../img/sprite-new.png?v=md5-hash);
-    2. 生成js，css 文件内容md5映射，可用于更新或者回滚版本号。
+    1. 替换背景图片地址 `background-image: url(../../img/sprite-new.png?v=md5-hash);`
+    2. 生成js，css 文件内容md5映射(`md5_mapping.json`)，可用于更新或者回滚版本号。
 
 #通过npm安装:
   npm install ria-packager 
+
+#usage:
+ `node index.js -from ~/code/litb-fe-prototype/src/ -to /tmp/dist/ -v `
--- index.js
+++ index.js
@@ -38,7 +38,7 @@ to = path.join(path.resolve(to),path.sep);
 //获得打包路径的列表
 console.log('Please wait...\n');
 
-var files = walk(from), jsList = files.js, cssList = files.css, otherFiles = files.other;
+var files = walk(from), jsList = files.js, cssList = files.css, htmlList = files.html, otherFiles = files.other;
 
 //1. 先把目标目录建立好
 var target;
@@ -52,7 +52,7 @@ jsList.concat(cssList).concat(otherFiles).forEach(function(uri){
 //2. 复制非js,css文件(swf,图片等静态资源,同时计算其md5)
 console.log('Copy files. Please wait...\n');
 otherFiles.forEach(function(source){
-	cpf(source,source.replace(from,to));
+	cpf(source,source.replace(from,to),to);
 });
 
 //3.压缩,合并css
@@ -63,6 +63,11 @@ weiboPkgJs(from,to,jsList,conf);
 
 //5 write md5Mapping.json
 writeMappingFile(from,to);
+
+//6 copy .html file,and replace the md5 version num.
+htmlList.forEach(function(source){
+	cpf(source,source.replace(from,to),to);
+});
 
 console.log('######## Package SUCCESS! ###########');
 console.timeEnd('Package-Time');
