5597ab03791caed84f14973c0f0de6833c0c2f4c
dongyuwei <newdongyuwei@gmail.com>
2012-11-30 19:51:45 +0800

    适应兰亭的开发环境

--- lib/server/restart.sh
+++ lib/server/restart.sh
@@ -3,7 +3,6 @@
 
 BASEDIR=$(dirname $0)
 PID=$BASEDIR/.pid
-DeployPID=$BASEDIR/deploy/.pid
 
 if [ -f $PID ]
 then
@@ -11,28 +10,16 @@ then
 	echo ' killed'
     cat $PID | xargs kill
 else
-    pkill -f weibo-ria-server
+    pkill -f ria-server
 fi
 
-if [ -f $DeployPID ]
-then
-	cat $DeployPID
-	echo ' killed'
-	
-    cat $DeployPID | xargs kill
-else
-    pkill -f ria-deploy-server
-fi
 
 # 启动服务器; 服务器日志默认定向到server目录下log.txt
-nohup node ${BASEDIR}/httpd.js  > ${BASEDIR}/log.txt 2>&1 &
-
-nohup node ${BASEDIR}/deploy/server.js  > ${BASEDIR}/deploy/log.txt 2>&1 &
+nohup node ${BASEDIR}/httpd.js  > /tmp/ria-server-log.txt 2>&1 &
 
 
 echo 'waiting...'
 sleep 5
-cat ${BASEDIR}/log.txt
-cat ${BASEDIR}/deploy/log.txt
+cat /tmp/ria-server-log.txt 
 
 exit 0
