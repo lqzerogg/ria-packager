fba3f7c1ad5a3ea9bf642d12c9bc384044cb63dc
dongyuwei <newdongyuwei@gmail.com>
2012-12-01 11:54:57 +0800

    default 8888 port

--- lib/server/restart.sh
+++ lib/server/restart.sh
@@ -15,11 +15,11 @@ fi
 
 
 # 启动服务器; 服务器日志默认定向到server目录下log.txt
-nohup node ${BASEDIR}/httpd.js  > /tmp/ria-server-log.txt 2>&1 &
+nohup node ${BASEDIR}/httpd.js  > ${BASEDIR}/.log.txt 2>&1 &
 
 
 echo 'waiting...'
 sleep 5
-cat /tmp/ria-server-log.txt 
+cat ${BASEDIR}/.log.txt 
 
 exit 0
