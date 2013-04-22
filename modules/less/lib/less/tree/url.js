c3b347df251193f7808c56387d8bfb5a2d4e20aa
dongyuwei <newdongyuwei@gmail.com>
2013-04-22 16:08:28 +0800

    less中背景图片自动加md5版本号

--- modules/less/lib/less/tree/url.js
+++ modules/less/lib/less/tree/url.js
@@ -1,5 +1,6 @@
 (function (tree) {
     var path = require('path');
+    var md5Mapping = require('../../../../../lib/tools/md5.js').getMD5Mapping();
 
     /**
      * 计算图片在合并后的文件中相对路径: 先根据图片和父级css的相对路径计算出图片路径,然后计算该路径与顶级css路径的相对路径.
@@ -10,6 +11,15 @@
     function relative(rootPath,imported,imgPath){
         //解决windows平台下path.relative带来的bug(先替换全部'\'为'/', 再去掉一个 '../' )
         return path.relative(rootPath,path.resolve(imported,'..',imgPath)).replace(/\\/g,'/').replace('../','');
+    }
+
+    /**
+     * 计算图片绝对路径
+     * @param{String}imported: @import引用的css路径
+     * @param{String}imgPath: @import引用的css文件中原始图片路径
+     * */
+    function absolute(imported,imgPath){
+        return path.resolve(path.dirname(imported),imgPath);
     }
 
     tree.URL = function (val, rootpath, env) {
@@ -35,7 +45,12 @@
             }
             
             var config = this.env.files._config_, host;
+            var img = absolute(this.env.filename, val.value).replace(/\\/g,'/');
             val.value = relative(config['_root_less_'], this.env.filename, val.value);
+            if(md5Mapping[img]){
+                val.value = val.value + '?v=' + md5Mapping[img];
+            }
+            
             return new(tree.URL)(val, this.rootpath);
         }
     };
